0c3ca5747e0b3b784bb087d9be09b52c77d458ee
Eliza <seeubanks12@gmail.com>
2022-01-31 14:12:51 -0500

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  //create new objects with 2 properties, items (array) and length (number of items in the array)
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    //pass the value "item" into the items array
+    //items stay sorted in ascending order
+    this.items.push(item);
+    this.items = this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    //get the value at index pos in the list - "throw" and error message is user tries element in non-existing position
+    if (pos > this.length - 1) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    //show the highest value
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max.apply(null, this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    //show the lowest value
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min.apply(null, this.items);
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => {
+      return a + b;
+    }, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length) {
+      return this.sum() / this.length;
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 }
 
 module.exports = SortedList;
